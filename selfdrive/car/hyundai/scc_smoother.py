059f6f2078d68da2a1bf46a5ad58a9b869f439aa
agegold <66872114+agegold@users.noreply.github.com>
2021-09-03 11:52:43 +0900

    Update scc_smoother.py

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -211,8 +211,8 @@ class SccSmoother:
     if not ascc_enabled:
       self.reset()
 
-    self.cal_target_speed(accel, CC, CS, clu11_speed, controls)
-
+    self.cal_target_speed(CS, clu11_speed, controls)      
+    
     CC.sccSmoother.logMessage = max_speed_log
 
     if self.wait_timer > 0:
@@ -308,8 +308,8 @@ class SccSmoother:
       else:
         self.curve_speed_ms = 255.
 
-  def cal_target_speed(self, accel, CC, CS, clu11_speed, controls):
-
+  def cal_target_speed(self, CS, clu11_speed, controls):        
+  
     if not self.longcontrol:
       if CS.gas_pressed and self.sync_set_speed_while_gas_pressed and CS.cruise_buttons == Buttons.NONE:
         if clu11_speed + SYNC_MARGIN > self.kph_to_clu(controls.v_cruise_kph):
